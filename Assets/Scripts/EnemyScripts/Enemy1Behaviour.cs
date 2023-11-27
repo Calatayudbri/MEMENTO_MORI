a521c07a53dd8d3bb6c44f3dd282f5de2b4d93a2
cmazcu <carlosmazcu@gmail.com>
2023-11-27 20:48:00 +0100

    enemies

---
+++ Assets/Scripts/EnemyScripts/Enemy1Behaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Behaviour : MonoBehaviour
+{
+  public int healthPoints;
+  public float speed;
+  public float knockBackForceX;
+  public float knockBackForceY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+  Enemy1Behaviour enemy;
+
+  public bool isDamage;
+  SpriteRenderer sp;
+  EnemyBlink blink;
+  Rigidbody2D rb;
+  private void Start()
+  {
+    sp = GetComponent<SpriteRenderer>();
+    rb = GetComponent<Rigidbody2D>();
+    blink = GetComponent<EnemyBlink>();
+    enemy = GetComponent<Enemy1Behaviour>();
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if(collision.CompareTag("Player_weapon") && !isDamage)
+    {
+      enemy.healthPoints -= 1;
+      if(collision.transform.position.x < transform.position.x)
+      {
+        rb.AddForce(new Vector2 (enemy.knockBackForceX, enemy.knockBackForceY), ForceMode2D.Force);
+      }else{
+        rb.AddForce(new Vector2 (-enemy.knockBackForceX, enemy.knockBackForceY), ForceMode2D.Force);
+
+      }
+      StartCoroutine(Damager());
+    }
+    if(enemy.healthPoints <= 0)
+    {
+      Destroy(gameObject);
+    }
+  }
+
+  IEnumerator Damager()
+  {
+    isDamage = true;
+    sp.material = blink.blink;
+
+    yield return new WaitForSeconds(0.5f);
+    isDamage = false;
+
+    sp.material = blink.original;
+
+
+
+  }
+}
