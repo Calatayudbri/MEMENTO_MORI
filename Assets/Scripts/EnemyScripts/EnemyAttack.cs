e7f452d1685105682e0238afd8dd055b264efaac
mazcunyanbla <mazcunyanbla@esat-miralles.com>
2023-12-04 11:55:41 +0100

    Lucas and Carlos: fix fireball

--- Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public GameObject fireballPrefab;  // Prefab de la bola de fuego
-    public float fireballSpeed = 8f;   // Velocidad de la bola de fuego
-    public float attackInterval = 2f;  // Intervalo entre ataques en segundos
+    public GameObject fireballPrefab;  
+    public float fireballSpeed = 8f;  
+    public float attackInterval = 2f; 
     float destroyBullet = 2.0f;
     float currentTime = 0.0f;
     float shootTime = 5.0f;
     public float speedShoot = 50.0f;
     public Transform initShootPoint;
+    public PlayerManager player_manager;
+    Vector2 bullet_dir;
 
+  
     void Update()
     {
+        bullet_dir = new Vector2(player_manager.transform.position.x - transform.position.x, transform.position.y);
+
         currentTime += Time.deltaTime;
         if (currentTime >= shootTime)
         {
@@ -22,13 +27,15 @@ public class EnemyAttack : MonoBehaviour
             currentTime = 0.0f;
         }
 
+
     }
 
     void Attack()
     {
         GameObject fireball = Instantiate(fireballPrefab, initShootPoint.position, Quaternion.Euler(0, 0, 0));
-        Rigidbody rb = fireball.GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * speedShoot;
+        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+        rb.velocity = bullet_dir * speedShoot;
+        
         Destroy(fireball, destroyBullet);
     }
 }
